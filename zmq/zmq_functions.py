cdfdecbffe22739626df648b49b27fd3d791b4fb
Ronny Schneeberger <ronny.schneeberger@students.fhnw.ch>
2023-03-15 15:08:53 +0100

    removed unnecessary function

--- zmq/zmq_functions.py
+++ zmq/zmq_functions.py
@@ -4,7 +4,3 @@ import json
 
 def timestamp():
     return datetime.now().strftime("%d-%m-%Y %H:%M:%S")
-
-def send_message(message:dict, socket:zmq.sugar.socket.Socket):
-    message = json.dumps(message).encode('utf-8')
-    socket.send(message)
