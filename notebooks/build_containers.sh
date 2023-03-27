98f2abc33d14873dc326e6b6f9f018e53b859e97
Ronny Schneeberger <ronny.schneeberger@students.fhnw.ch>
2023-03-27 12:01:22 +0200

    no longer necessary

--- notebooks/build_containers.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-docker build -f DockerFile.binance_processor -t binance_processor:latest .
-docker build -f DockerFile.binance_producer -t binance_producer:latest .
-docker build -f DockerFile.twitter_processor -t twitter_processor:latest .
-docker build -f DockerFile.twitter_producer -t twitter_producer:latest .
